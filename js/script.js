c198115bb9b2cdfc99dd94ea89e50af9082915c6
Laura <lstorella10@gmail.com>
2020-05-19 15:08:04 +0200

    JS - Password Generator

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Richiesta Nome Utente
+var nomeUtente = prompt('Inserisci il tuo nome (in minuscolo)');
+console.log(nomeUtente);
+
+// Richiesta Cognome Utente
+var cognomeUtente = prompt('Inserisci il tuo cognome (in minuscolo)');
+console.log(cognomeUtente);
+
+// Richiesta colore preferito Utente
+var coloreUtente = prompt('Inserisci il tuo colore preferito (in minuscolo)');
+console.log(coloreUtente);
+
+// Richiesta anno corrente
+var annoCorrente = prompt('Inserisci le ultime due cifre dell anno in corso');
+console.log(annoCorrente);
+
+// Genera Password
+var psdUtente = nomeUtente+cognomeUtente+coloreUtente+annoCorrente;
+console.log(psdUtente);
+document.getElementById('psdGenerator').innerHTML = psdUtente;
+
+// Conferma Login Utente
+alert('Login esuguito con successo!');
+
+// Saluto Utente
+var salutoUtente = 'Ciao ' + nomeUtente + '! Benvenuto/a sul tuo profilo.'
+console.log(salutoUtente);
+document.getElementById('welcome').innerHTML = salutoUtente;
